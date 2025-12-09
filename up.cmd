fa7f78e5f824a65749b9d04c434fb3840ed4c7fc
profjosimar <josimar.ribeiro@prof.eteprc.faetec.rj.gov.br>
2025-12-09 06:11:59 -0300

    first commit

--- up.cmd
+++ up.cmd
@@ -4,3 +4,5 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin git@github.com:josimaribeiro/ciep_spring.git
 git push -u origin main
+
+::ssh-keygen -t ed25519 -C "ceatseeduc@gmail.com"
